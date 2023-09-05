bbdc7e39a92544fc74e30433c0ab9fbef5815f24
Şamil Nugay <samil_nugay@hotmail.com>
2023-09-05 21:11:39 +0300

    usestate and click events

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  const headerStyle = { background: "royalblue", color: "#fff" };
+
+  return (
+    <header style={headerStyle}>
+      <h1>Grocceries List</h1>
+    </header>
+  );
+};
+
+export default Header;
